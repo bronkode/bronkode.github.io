a03558261574bc0ab9aa085b855c3687b9493796
Theuns Snyman <theuns@mcAir.local>
2017-10-01 22:20:03 +0200

    fixed bk.js

--- js/bk.js
+++ js/bk.js
@@ -1,8 +1,7 @@
 const h1Els = document.querySelectorAll("h1");
-h1Els.forEach(h => {
-    h.onclick = navHome();
-});
+h1Els.forEach(h => makeLink(h,"http://blog.bronkode.co.za"));
 
-function navHome(){
-    window.location = "http;//blog.bronkode.co.za";
+function makeLink(el,uri){
+    el.style.cursor = "pointer";
+    el.onclick = ()=> {window.location.href = uri;};
 }
