7d5e18480a1b9926b7d78a55b0b219ca58ad9b04
Theuns Snyman <theuns@mcAir.local>
2017-10-01 23:17:32 +0200

    added string literal template

--- js/bk.js
+++ js/bk.js
@@ -1,7 +1,22 @@
-const h1Els = document.querySelectorAll("h1");
-h1Els.forEach(h => makeLink(h,"http://blog.bronkode.co.za"));
+document.querySelectorAll("h1")
+.forEach(h => makeLink(h,"http://blog.bronkode.co.za"));
+
+document.querySelectorAll(".articleDate")
+.forEach(d => fillTemplate(d));
+
 
 function makeLink(el,uri){
     el.style.cursor = "pointer";
     el.onclick = ()=> {window.location.href = uri;};
 }
+
+function fillTemplate(el){
+    let expr = /\`(\s*?.*?)*?\`/g;
+    let tmpHtml = el.innerHTML;
+    let match = tmpHtml.match(expr);
+    match.forEach(m => tmpHtml = tmpHtml.replace(m,eval(m)));
+    el.innerHTML = tmpHtml;
+    
+}
+
+
